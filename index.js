8b07b5faab0b3cfde79ec46ae9e698d14a8e9444
FieryHacker <34077488+FieryHacker@users.noreply.github.com>
2018-10-25 15:09:24 -0400

    Updated index.js
    added simple commands

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 //Setting up file
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const store = require(./index.json);
 
 client.on('ready', () => {
   console.log('Logged in as ${client.user.tag}!');
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
+  if (msg.content === '${store.prefix}ping') {
     msg.reply('Pong!');
   }
+  
+  if(message.content === '${store.prefix}help') {
+     new Discord.RichEmbed();
+  };
+  
 });
 
-if
-
 //Reminder: put token here
-client.login('');
+client.login('store.token');
