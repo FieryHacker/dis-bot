35d3abcf8a62a3770d90d6d307da6a7ec712da7a
FieryHacker <34077488+FieryHacker@users.noreply.github.com>
2018-10-25 14:46:41 -0400

    Updated index.js
    Added variables and start of discord.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+//Setting up file
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
+client.on('ready', () => {
+  console.log('Logged in as ${client.user.tag}!');
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
+if
+
+//Reminder: put token here
+client.login('');
