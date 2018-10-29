5d48fbaa74c26d094f4a06084370d03f8dd83b29
FieryHacker <34077488+FieryHacker@users.noreply.github.com>
2018-10-29 09:32:23 -0400

    Update index.js
    Added comments to organize code

--- index.js
+++ index.js
@@ -3,15 +3,18 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const store = require(./index.json);
 
+//When the bot loads up on Discord
 client.on('ready', () => {
   console.log('Logged in as ${client.user.tag}!');
 });
 
 client.on('message', msg => {
+  //Ping command
   if (msg.content === '${store.prefix}ping') {
     msg.reply('Pong!');
   }
   
+  //Help command
   if(message.content === '${store.prefix}help') {
      new Discord.RichEmbed();
   };
